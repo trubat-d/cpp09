7fc98e3e20cffd44c2d952aff52bd44fc31c229f
trubat-d <theotime2015@gmail.com>
2024-01-31 14:12:48 +0100

    WOW

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,179 +1,54 @@
-#include "PmergeMe.h"
+#include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe() : _k(0){}
-
-PmergeMe::PmergeMe(int k) : _k(k){}
-
-PmergeMe::~PmergeMe(){}
-
-PmergeMe::PmergeMe(const PmergeMe &src) : _k(src._k){}
-
-PmergeMe &PmergeMe::operator=(const PmergeMe &src){(void)src;return (*this);}
-
-void PmergeMe::list_insert(std::list<int> & lst)
-{
-	if(lst.size() <= 1)
-		return ;
-	std::list<int>::iterator it = lst.begin();
-	std::list<int>::iterator itn = std::next(it,1);
-	std::list<int>::iterator ite = lst.end();
-	while(itn != ite)
-	{
-		while(it != ite  && *it < *itn)
-			it++;
-		while(it != ite && *it == *itn)
-			it++;
-		if(it != ite && *it > *itn)
-		{
-			std::list<int>::iterator tmp = itn;
-			int tmp_val = *itn;
-			tmp++;
-			lst.erase(itn);
-			lst.insert(it, tmp_val);
-			itn = tmp;
-		}
-		else
-			itn++;
-		it = lst.begin();
-	}
+bool         PmergeMe::args_handler(char** args) {
+	for(int i = 0; args[i]; i++)
+		for(int j = 0; args[i][j]; j++)
+			if(args[i][j] < '0' || args[i][j] > '9')
+				return false;
+	return true;
 }
 
-void PmergeMe::list_sort(std::list<int> & lst, int st, int end)
-{
-	if(static_cast<int>(lst.size()) > this->_k)
-	{
-		int mid = lst.size()/2;
-		std::list<int> first_half;
-		first_half.splice(first_half.begin(), lst, lst.begin(), std::next(lst.begin(), mid));
-		list_sort(first_half, st, mid);
-		std::list<int> sec_half;
-		sec_half.splice(sec_half.begin(), lst,  std::next(lst.begin(), mid+1), lst.end());
-		list_sort(sec_half, mid+1, end);
-		lst = list_merge(first_half, sec_half);
-	}
-	else
-	{
-		list_insert(lst);
-	}
+int          PmergeMe::jacobsthal(int index) {
+	if (index == 0) return 0;
+	else if (index == 1) return 1;
+	else return (jacobsthal(index-1) + 2 * jacobsthal(index-2));
 }
 
-std::list<int> PmergeMe::list_merge(std::list<int> &ar_fir, std::list<int> &ar_sec)
+std::ostream& operator<<(std::ostream& os, std::vector<int> & ref)
 {
-	std::list<int> lst;
-	std::list<int>::iterator it_f = ar_fir.begin();
-	std::list<int>::iterator ite_f = ar_fir.end();
-	std::list<int>::iterator it_s = ar_sec.begin();
-	std::list<int>::iterator ite_s = ar_sec.end();
-
-	while(it_f != ite_f || it_s != ite_s)
-	{
-		if(it_f == ite_f)
-		{
-			lst.push_back(*it_s);
-			it_s++;
-		}
-		else if (it_s == ite_s)
-		{
-			lst.push_back(*it_f);
-			it_f++;
-		}
-		else
-		{
-			if(*it_f <= *it_s)
-			{
-				lst.push_back(*it_f);
-				it_f++;
-			}
-			else
-			{
-				lst.push_back(*it_s);
-				it_s++;
-			}
-		}
-	}
-
-	return (lst);
+	for(std::vector<int>::iterator it = ref.begin(); it != ref.end(); it++)
+		os << *it << " ";
+	return os;
 }
 
-void PmergeMe::vect_insert(std::vector<int> & lst)
+std::ostream& operator<<(std::ostream& os, std::list<int> & ref)
 {
-	if(lst.size() <= 1)
-		return ;
-	std::vector<int>::iterator it = lst.begin();
-	std::vector<int>::iterator itn = std::next(it,1);
-	std::vector<int>::iterator ite = lst.end();
-	while(itn != ite)
-	{
-		while(it != ite && *it < *itn)
-			it++;
-		while(it != ite && *it == *itn)
-			it++;
-		if(it != ite && *it > *itn)
-		{
-			std::vector<int>::iterator tmp = itn;
-			int tmp_val = *itn;
-			tmp++;
-			lst.erase(itn);
-			lst.insert(it, tmp_val);
-			itn = tmp;
-		}
-		else
-			itn++;
-		it = lst.begin();
-	}
+	for(std::list<int>::iterator it = ref.begin(); it != ref.end(); it++)
+		os << *it << " ";
+	return os;
 }
 
-void PmergeMe::vect_sort(std::vector<int> & lst, int st, int end)
+std::ostream& operator<<(std::ostream& os, std::vector<std::pair<int,int> > & ref)
 {
-	if(static_cast<int>(lst.size()) > this->_k)
-	{
-		int mid = lst.size()/2;
-		std::vector<int> first_half(lst.begin(), std::next(lst.begin(), mid));
-		vect_sort(first_half, st, mid);
-		std::vector<int> sec_half(std::next(lst.begin(), mid), lst.end());
-		vect_sort(sec_half, mid, end);
-		lst = vect_merge(first_half, sec_half);
-	}
-	else
-	{
-		vect_insert(lst);
-	}
+	for(std::vector<std::pair<int, int> >::iterator it = ref.begin(); it != ref.end(); it++)
+		os << it->first << " " << it->second << " ";
+	return os;
 }
 
-std::vector<int> PmergeMe::vect_merge(std::vector<int> &ar_fir, std::vector<int> &ar_sec)
+std::ostream& operator<<(std::ostream& os, std::list<std::pair<int, int> > & ref)
 {
-	std::vector<int> lst;
-	std::vector<int>::iterator it_f = ar_fir.begin();
-	std::vector<int>::iterator ite_f = ar_fir.end();
-	std::vector<int>::iterator it_s = ar_sec.begin();
-	std::vector<int>::iterator ite_s = ar_sec.end();
+	for(std::list<std::pair<int, int> >::iterator it = ref.begin(); it != ref.end(); it++)
+		os << it->first << " " << it->second << " ";
+	return os;
+}
 
-	while(it_f != ite_f || it_s != ite_s)
-	{
-		if(it_f == ite_f)
-		{
-			lst.push_back(*it_s);
-			it_s++;
-		}
-		else if (it_s == ite_s)
-		{
-			lst.push_back(*it_f);
-			it_f++;
-		}
-		else
-		{
-			if(*it_f <= *it_s)
-			{
-				lst.push_back(*it_f);
-				it_f++;
-			}
-			else
-			{
-				lst.push_back(*it_s);
-				it_s++;
-			}
-		}
-	}
 
-	return (lst);
+const char *PmergeMe::WrongArgsException::what() const throw()
+{
+	return ("Error: Args content must only be numbers");
+}
+
+const char *PmergeMe::WrongContainerException::what() const throw()
+{
+	return ("Error: Make sure to use at least one of those container : vector or list !");
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,48 +1,47 @@
-#include "PmergeMe.h"
+#include "PmergeMe.hpp"
+#include <time.h>
 
 int main(int ac, char **av)
 {
-	if(ac < 2)
+
+	if(ac <= 1)
 	{
-		std::cout << "Send at least a list of two positive integers" << std::endl;
-		return (1);
+		std::cerr << "need more inputs" << std::endl;
+		return 1;
 	}
-	std::list<int> lst;
-	std::vector<int> vect;
-	vect.reserve(ac+1);
-	for(int i = 1; i < ac; i++)
 	{
-		int test = atoi(av[i]);
-		if(test <= 0)
+		try
 		{
-			std::cout << "Only positive integers are allowed" << std::endl;
-			return (1);
+			std::cout << "Before: ";
+			for (int i = 1; i < ac; i++)
+				std::cout << av[i] << " ";
+			std::cout << std::endl;
+			std::clock_t start = std::clock();
+			std::list<int> sorted_list = PmergeMe::ford_jhonson<std::list>(av);
+			std::clock_t end = std::clock();
+			std::cout << "After: ";
+			std::cout << sorted_list << std::endl;
+			// std::cout << std::endl;
+			std::cout << std::endl;
+			std::cout << "Time to process a range of " << sorted_list.size() << " elements with std::list<int> : " << ((double) (end - start)) / CLOCKS_PER_SEC << "us" << std::endl;
 		}
-		lst.push_back(test);
-		vect.push_back(test);
+		catch (const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 	}
-	PmergeMe merger(5);
-
-	std::cout << "Before : ";
-	for(std::list<int>::iterator it = lst.begin(), end=lst.end(); it != end; it++)
 	{
-		std::cout << *it << " ";
+		try
+		{
+			std::clock_t start = std::clock();
+			std::vector<int> sorted_vector = PmergeMe::ford_jhonson<std::vector>(av);
+			std::clock_t end = std::clock();
+			std::cout << "Time to process a range of " << sorted_vector.size() << " elements with std::vector<int> : " << ((double) (end - start)) / CLOCKS_PER_SEC << "us" << std::endl;
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 	}
-	std::cout << std::endl;
-
-	const clock_t beginlist = std::clock();
-	merger.list_sort(lst, 0, ac-1);
-	double endlist = static_cast<double> (std::clock() - beginlist) / CLOCKS_PER_SEC;
-	const clock_t beginvect = std::clock();
-	merger.vect_sort(vect, 0, ac-1);
-	double endvect = static_cast<double> (std::clock() - beginvect) / CLOCKS_PER_SEC;
-
-	std::cout << "After : ";
-	for(std::vector<int>::iterator it = vect.begin(), end=vect.end(); it != end; it++)
-	{
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;
-	std::cout << std::fixed << "Time to process a range of " << ac-1 << " elements with std::[list] : "  << endlist << "us" << std::endl;
-	std::cout << std::fixed << "Time to process a range of " << ac-1 << " elements with std::[vector] : "  << endvect << "us" << std::endl;
+	return 0;
 }
